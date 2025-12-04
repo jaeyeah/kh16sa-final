ad9bb9abcba86ce4db46a3e1426aaa51a4550275
gwcloud72 <uu7271gw@gmail.com>
2025-12-04 14:18:04 +0900

    포인트내역 Mybatis맵퍼 Dao Dto생성

---
+++ finalproject/src/main/java/com/kh/finalproject/dao/PointHistoryDao.java
@@ -0,0 +1,45 @@
+package com.kh.finalproject.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.finalproject.dto.PointHistoryDto;
+
+@Repository
+public class PointHistoryDao {
+@Autowired
+private SqlSession sqlSession;
+
+//1.등록 C
+public void insert(PointHistoryDto pointHistoryDto)
+{ 
+	sqlSession.insert("pointhistory.insert",pointHistoryDto);
+}
+//2 수정 U
+public boolean update(PointHistoryDto pointHistoryDto)
+{
+	return sqlSession.update("pointhistory.update",pointHistoryDto)>0;
+	
+}
+//3.(1).회원아이디 기준으로 전체출력
+public List<PointHistoryDto> selectList()
+{
+	return sqlSession.selectList("pointhistory.selectList");
+}
+//3.(2).번호기준조회
+public PointHistoryDto selectOneNumber(int pointHistoryNo)
+{
+  return sqlSession.selectOne("pointhistory.selectOneNumber", pointHistoryNo);
+}
+
+//4..삭제
+ 
+public boolean delete(int pointHistoryNo)
+{
+	return sqlSession.delete("pointhistory.delete",pointHistoryNo)>0;
+}
+
+}
---
+++ finalproject/src/main/java/com/kh/finalproject/dto/PointHistoryDto.java
@@ -0,0 +1,27 @@
+package com.kh.finalproject.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class PointHistoryDto {
+    
+    // DB컬럼: point_history_no -> 자바: pointHistoryNo
+    private int pointHistoryNo; 
+    
+    // DB컬럼: point_history_member_id -> 자바: pointHistoryMemberId
+    private String pointHistoryMemberId; 
+    
+    // DB컬럼: point_history_amount -> 자바: pointHistoryAmount
+    private int pointHistoryAmount; 
+    
+    // DB컬럼: point_history_reason -> 자바: pointHistoryReason
+    private String pointHistoryReason; 
+    
+    // DB컬럼: point_history_date -> 자바: pointHistoryDate
+    private LocalDateTime pointHistoryDate; 
+}
